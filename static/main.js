c899eed94751a98d863928b59c62aa0543019685
WouterPennigs <wouterpennings@gmail.com>
2021-10-26 18:04:51 +0200

    feat: school year auto increase

--- static/main.js
+++ static/main.js
@@ -4,3 +4,18 @@ function GetSubtitle(){
     const text = `${(currentYear - birth)} Years // Dutch`;
     document.write(text);
 }
+
+function GetDiscription() {
+    let insert = "";
+    const start = parseInt(new Date('August 01, 2020 00:01').getFullYear());
+    const current = parseInt(new Date().getFullYear());
+    console.log(current)
+    let x = current - start;
+    console.log(x);
+    if(x === 1) insert = "sophomore";
+    else if(x === 2) insert = "junior";
+    else if(x === 3) insert = "senior";
+    else if(x > 4) insert = "graduated";
+
+    document.write(`I am a ${insert} computer science student at Fonty's University of Applied Sciences.`)
+}
